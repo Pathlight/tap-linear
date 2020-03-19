6f59e08e03f50bb771dc70aea5505e1bca2861ca
Jacob Chan <jacob@pathlight.com>
2020-03-19 15:05:20 -0400

    linear tap initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-linear",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_linear"],
+    install_requires=[
+        "singer-python>=5.0.12",
+        "gql"
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-linear=tap_linear:main
+    """,
+    packages=["tap_linear"],
+    package_data = {
+        "schemas": ["tap_linear/schemas/*.json"]
+    },
+    include_package_data=True,
+)
---
+++ tap_linear/client.py
@@ -0,0 +1,66 @@
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+import json
+
+MAX_RESULTS = 50
+
+class GraphQLClient:
+    def __init__(self, config):
+        self.api_key = config['api_key']
+        self.base_url = config['base_url']
+        sample_transport=RequestsHTTPTransport(
+            url=self.base_url,
+            use_json=True,
+            headers={
+                "Content-type": "application/json",
+                "Authorization": self.api_key
+            },
+            verify=False
+        )
+        self.client = Client(
+            retries=3,
+            transport=sample_transport,
+            fetch_schema_from_transport=True,
+        )
+        
+    def get(self, name, schema, is_singular=False):
+        # `is_singular` is a way to deal with Linear's implementation of 'connections'.
+        done = False
+        after_id = None
+        final_result = []
+        while not done:
+            query = gql(self.schema_to_gql(name, schema, is_singular, after_id))
+            res = self.client.execute(query)
+            if is_singular:
+                final_result = [res[name]]
+                done = True 
+            else:
+                final_result = final_result + res[name]['nodes']
+                if len(res[name]['nodes']) >= MAX_RESULTS:
+                    after_id = res[name]['nodes'][-1]['id']
+                else:
+                    done = True
+        return final_result
+    def schema_to_gql(self, root_name, schema, is_singular, after_id=None):
+        def properties_to_gql(name, value, is_singular):
+            if 'properties' in value:
+                children = ''
+                for k,v in value['properties'].items():
+                    # determining if this object will be a `connection` in Linear's API.
+                    if len(v["type"]) > 1 and v["type"][1] == "array" and "items" in v:
+                        children += f'{properties_to_gql(k, v["items"], False)} '
+                    else:
+                        children += f'{properties_to_gql(k, v, True)} '
+                # hack to deal with Linear's implementation of 'connections'.
+                if is_singular:
+                    children = f'{{ {children} }}'
+                else:
+                    children = f'{{ nodes {{ {children} }} }}'
+                if after_id and name == root_name:
+                    name += f'(first: {MAX_RESULTS},after: "{after_id}")'
+                return f'{name} {children}'
+            else:
+                return f'{name} '
+        gql_properties = properties_to_gql(root_name, schema, is_singular)
+        gql_string = f'''query {{ {gql_properties} }}'''
+        return gql_string
